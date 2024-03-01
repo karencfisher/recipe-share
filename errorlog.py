28fd545ad8f8fd64ae1af50177b5c5732986b91f
karencfisher <karencfisher@hotmail.com>
2024-02-29 17:13:46 -0700

    Reorder a couple of pages

--- errorlog.py
+++ errorlog.py
@@ -5,8 +5,7 @@ from pathlib import Path
 class ErrorLog:
     def __init__(self):
         logfile = Path("logs/error-log.log")
-        logging.basicConfig(handlers=[logging.FileHandler(logfile, 'a', 'utf-8')], 
-                            level=logging.INFO, 
+        logging.basicConfig(level=logging.INFO, 
                             format='%(asctime)s %(message)s')
         self.logger = logging.getLogger('error_log')
 

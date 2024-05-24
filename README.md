8ef801fffb07e5ff6cd167cfaf0f2b3d9b7beb8c
karencfisher <karencfisher@hotmail.com>
2024-05-24 17:26:05 -0600

    added login page

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # recipe-share
+
+This site is a demo of a web application for finding and sharing recipes. You
+can search for recipes in the database, or add your own (at present, anonymously).
+This is a preliminary release, so it will hopefully become more featured in 
+the not too distant future. 
---
+++ static/login.js
@@ -0,0 +1,51 @@
+const selection = [...document.getElementsByClassName("select")];
+selection.forEach((item) => {
+    item.addEventListener("change", (e) => {
+        const emailField = document.getElementById("email-div");
+        const passwordConfirm = document.getElementById("password-confirm");
+        const forgotButton = document.getElementById("forgot-button");
+        const loginButton = document.getElementById("login-button");
+
+        if (e.target.id === "register-select") {
+            emailField.style.setProperty("display", "block");
+            passwordConfirm.style.setProperty("display", "flex");
+            forgotButton.style.setProperty("display", "none")
+            loginButton.innerText = "Register";
+        }
+        else {
+            emailField.style.setProperty("display", "none");
+            passwordConfirm.style.setProperty("display", "none");
+            forgotButton.style.setProperty("display", "block");
+            loginButton.innerText = "Login";
+        }
+    });
+});
+
+const eyes = [...document.getElementsByClassName("eyes")];
+eyes.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        const passwordText = document.getElementById("password-text");
+        const passwordConfirmText = document.getElementById("password-confirm-text");
+        if (e.target.id === "eye") {
+            if (e.target.innerText === "visibility") {
+                e.target.innerText = "visibility_off";
+                passwordText.setAttribute("type", "text");
+            }
+            else {
+                e.target.innerText = "visibility";
+                passwordText.setAttribute("type", "password");
+            }
+        }
+        else {
+            if (e.target.innerText === "visibility") {
+                e.target.innerText = "visibility_off";
+                passwordConfirmText.setAttribute("type", "text");
+            }
+            else {
+                e.target.innerText = "visibility";
+                passwordConfirmText.setAttribute("type", "password");
+            }
+        }
+    });
+});
+
